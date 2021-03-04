561b85bda049125a3a5f58fca53d95ad7a335236
Diana <dianasuica99@gmail.com>
2021-03-04 13:59:21 +0200

    added

--- sem1/src/ro/ase/csie/cts/seminar2/Main.java
+++ sem1/src/ro/ase/csie/cts/seminar2/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		CalendarUtil cu = new CalendarUtil();
 		
-		System.out.println(cu.weekDay(6));
+		System.out.println(cu.weekDay(3));
 
 	}
 
